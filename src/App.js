6d11ce42c3d6025125394bd6cecd3812f8fe47d6
MofasserHossain <mofasserhossain1221@gmail.com>
2021-02-25 22:10:28 +0600

    file upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const personName = [
+  { name: 'Mofasser Hossain', job: 'Web Designer & Developer' },
+  { name: 'Sayed Rabet', job: 'Automobile Engineer' },
+  { name: 'Asif Al Mahbub', job: 'Future Doctor' },
+  { name: 'Rakibul Islam', job: 'Graphics Designer' },
+  { name: 'Mohammad Rayhan', job: 'SEO & Digital Marketer' },
+  { name: 'Hafizul Islam Sakib', job: 'Digital Marketer' },
+  { name: 'Mahbub Hosan', job: 'Web Designer & Developer' },
+  { name: 'AH Nafiz', job: 'Web Designer' },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Title title="Friend List And Job"></Title>
+        <header className="App-header">
+          {personName.map((person) => (
+            <Person person={person}></Person>
+          ))}
+        </header>
+      </div>
     </div>
   );
+}
+
+function Person(props) {
+  console.log(props);
+  const { name, job } = props.person;
+  return (
+    <div className="grid">
+      <h3>Name : {name}</h3>
+      <span>job : {job}</span>
+    </div>
+  );
+}
+
+function Title(props) {
+  return <h3 className="title">{props.title}</h3>;
 }
 
 export default App;
