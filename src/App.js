026694ff84ff26e9ccc4e518cb58063230aa3089
MofasserHossain <mofasserhossain1221@gmail.com>
2021-02-26 19:23:54 +0600

    file modify

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-const personName = [
-  { name: 'Mofasser Hossain', job: 'Web Designer & Developer' },
-  { name: 'Sayed Rabet', job: 'Automobile Engineer' },
-  { name: 'Asif Al Mahbub', job: 'Future Doctor' },
-  { name: 'Rakibul Islam', job: 'Graphics Designer' },
-  { name: 'Mohammad Rayhan', job: 'SEO & Digital Marketer' },
-  { name: 'Hafizul Islam Sakib', job: 'Digital Marketer' },
-  { name: 'Mahbub Hosan', job: 'Web Designer & Developer' },
-  { name: 'AH Nafiz', job: 'Web Designer' },
-];
-
 function App() {
+  const personName = [
+    { name: 'Mofasser Hossain', job: 'Web Designer & Developer' },
+    { name: 'Sayed Rabet', job: 'Automobile Engineer' },
+    { name: 'Asif Al Mahbub', job: 'Future Doctor' },
+    { name: 'Rakibul Islam', job: 'Graphics Designer' },
+    { name: 'Mohammad Rayhan', job: 'SEO & Digital Marketer' },
+    { name: 'Hafizul Islam Sakib', job: 'Digital Marketer' },
+    { name: 'Mahbub Hasan', job: 'Web Designer & Developer' },
+    { name: 'AH Nafiz', job: 'Web Designer' },
+  ];
   return (
     <div className="App">
       <div className="container">
@@ -22,13 +21,30 @@ function App() {
             <Person person={person}></Person>
           ))}
         </header>
+        <Counter></Counter>
       </div>
     </div>
   );
 }
 
-function Person(props) {
-  console.log(props);
+const Counter = () => {
+  let [count, setCount] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Add Movie</button>
+      <button onClick={() => setCount(count - 1)}>Remove Movie</button>
+      <h5 style={{ color: 'white' }}>Number of movie : {count}</h5>
+      <DisplayMovie count={count * 2}></DisplayMovie>
+      <Friends></Friends>
+    </div>
+  );
+};
+
+const DisplayMovie = (props) => {
+  return <h3 style={{ color: 'white' }}>Movie Number : {props.count}</h3>;
+};
+
+const Person = (props) => {
   const { name, job } = props.person;
   return (
     <div className="grid">
@@ -36,10 +52,40 @@ function Person(props) {
       <span>job : {job}</span>
     </div>
   );
-}
+};
 
-function Title(props) {
-  return <h3 className="title">{props.title}</h3>;
-}
+const Title = (props) => {
+  return (
+    <h3 className="title" key="1">
+      {props.title}
+    </h3>
+  );
+};
+
+const Friends = () => {
+  // const friend = ['rabet', 'sakib', 'emon', 'asif'];
+  const [friends, setFriends] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setFriends(data);
+        // console.log(friends);
+      });
+  }, []);
+  return (
+    <div>
+      <h3>Users Number : {friends.length}</h3>
+      <ul style={{ listStyle: 'none', color: 'white' }}>
+        {friends.map((friendsName) => (
+          <div>
+            <li key={friendsName.id}> {friendsName.name}</li>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
